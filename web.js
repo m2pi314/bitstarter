654051ca2b67c45e19c0d78b4b12b3411881c6de
m2pi314 <plysacik@web.de>
2013-07-25 02:35:09 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,33 @@
+var fs     = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  /* read the file index.html*/
+  var buff = fs.readFileSync('index.html', function (err, data) {
+      if (err) throw err;
+      console.log(data);
+  });
+
+
+
+
+  var str = buff.toString('utf8',0,buff.length);  
+
+  response.send(str);
+
+
+
+
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+ 
+
